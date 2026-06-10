f703c2888c93c9f4aee389804389f0c216f9acb8
Juegos y mas <isaacclone8@gmail.com>
2026-06-10 00:19:42 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import Header from './components/Header';
+import BottomNav from './components/BottomNav';
+import SideNav from './components/SideNav';
+import HomePage from './pages/HomePage';
+import AppDetailPage from './pages/AppDetailPage';
+import CategoryPage from './pages/CategoryPage';
+import SearchPage from './pages/SearchPage';
+import CategoriesPage from './pages/CategoriesPage';
+import TopPage from './pages/TopPage';
+import FavoritesPage from './pages/FavoritesPage';
+import AdminPage from './pages/admin/AdminPage';
+import './App.css';
+
+function Layout({ children }) {
+  return (
+    <div className="app-layout">
+      <Header />
+      <div className="app-body">
+        <SideNav />
+        <main className="app-main">
+          {children}
+        </main>
+      </div>
+      <BottomNav />
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <HashRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/app/:id" element={<AppDetailPage />} />
+            <Route path="/category/:id" element={<CategoryPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/categories" element={<CategoriesPage />} />
+            <Route path="/top" element={<TopPage />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+          </Routes>
+        </Layout>
+      </HashRouter>
+    </ThemeProvider>
+  );
+}
